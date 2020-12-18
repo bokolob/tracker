64863f6aa97dba62717dc7118eb5f5c297afc40f
oxid <oxid@yandex-team.ru>
2020-12-18 19:34:56 +0300

    Incoming connections handler

---
+++ server/db/model.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+# mysql://username:password@server/db
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI');
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+# an example mapping using the base
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    devices = db.relationship('Device', backref='devices', lazy=True)
+
+
+class Device(db.Model):
+    __tablename__ = 'devices'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    imei = db.Column(db.String(16), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    user = db.relationship("User", back_populates="devices", lazy=True)
+    coordinates = db.relationship("Coordinates", back_populates="device", lazy=True)
+
+
+class Coordinates(db.Model):
+    __tablename__ = 'coordinates'
+    id = db.Column(db.BigInteger, primary_key=True)
+    device_id = db.Column(db.Integer, db.ForeignKey('devices.id'), nullable=False)
+    device = db.relationship("Device", back_populates="coordinates")
+    lat = db.Column(db.Numeric)
+    lng = db.Column(db.Numeric)
---
+++ server/incoming.py
@@ -0,0 +1,139 @@
+import socket
+import os
+import asyncio
+import logging
+import db.model
+import datetime
+import asyncio
+from urllib.parse import urlparse
+from aiomysql.sa import create_engine
+from sqlalchemy.dialects.mysql import insert
+from sqlalchemy.sql import select
+
+PROC_COUNT = 5
+BULK_SIZE = 500
+
+queue = asyncio.Queue()
+known_devices = dict()
+tmp = 0
+
+db.model.db.create_all()
+
+
+def create_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', 25000))
+    sock.listen()
+    sock.setblocking(False)
+    return sock
+
+
+async def get_device_id(imei, engine):
+    global known_devices
+    if known_devices.get(imei) is not None:
+        return known_devices.get(imei)
+
+    statement = select([db.model.Device.id]).select_from(db.model.Device).where(db.model.Device.imei == imei).limit(1)
+
+    async with engine.acquire() as conn:
+        result_set = await conn.execute(statement)
+        res = await result_set.fetchone()
+
+        if res is not None:
+            res = res.get("id")
+            known_devices[imei] = res
+
+        await result_set.close()
+
+        return res
+
+
+def get_id(device_id):
+    return (device_id << 32) | int(datetime.datetime.utcnow().timestamp() / 10)
+
+
+async def parse_input(line, engine):
+    print(line)
+    proto, imei, lat, lng, satelites, battery, status = line.split(b',')
+    device_id = await get_device_id(imei, engine)
+    return {'id': get_id(device_id),
+            'device_id': device_id,
+            'lat': lat,
+            'lng': lng}
+
+
+async def process_queue(engine):
+    buf = []
+    while True:
+        timeout = False
+        try:
+            coords = await asyncio.wait_for(queue.get(), timeout=15.0)
+            buf.append(coords)
+            queue.task_done()
+        except asyncio.TimeoutError:
+            timeout = True
+            pass
+
+        if len(buf) >= BULK_SIZE or (len(buf) > 0 and timeout):
+            async with engine.acquire() as conn:
+                trans = await conn.begin()
+
+                act = insert(db.model.Coordinates).values(buf)
+
+                on_duplicate_key_stmt = act.on_duplicate_key_update(
+                    {'lat': act.inserted.lat, 'lng': act.inserted.lng}
+                )
+
+                await conn.execute(on_duplicate_key_stmt)
+                await trans.commit()
+            buf = []
+
+
+async def handler(event_loop, client, engine):
+    print("handler" + str(os.getpid()))
+    with client:
+        buf = bytearray()
+
+        while True:
+            try:
+                data = await asyncio.wait_for(event_loop.sock_recv(client, 64), timeout=5)
+
+                if not data:
+                    break
+                else:
+                    buf += data
+                    if b'\n' in buf:
+                        coords = await parse_input(data, engine)
+                        await queue.put(coords)
+
+                        print(coords)
+
+            except asyncio.TimeoutError:
+                print("timeout")
+                break
+
+
+async def server_loop(loop, server_sock):
+    mysql_params = urlparse(db.model.app.config['SQLALCHEMY_DATABASE_URI'])
+    engine = await create_engine(user=mysql_params.username, db=mysql_params.path.strip("/"),
+                                 host=mysql_params.hostname, password=mysql_params.password, loop=loop)
+
+    print("ddd")
+    loop.create_task(process_queue(engine))
+
+    while True:
+        (conn, address) = await loop.sock_accept(server_sock)
+        loop.create_task(handler(loop, conn, engine))
+
+
+server_sock = create_server()
+
+# for num in range(PROC_COUNT):
+#    pid = os.fork()
+#    if pid <= 0:
+#        break
+
+loop = asyncio.get_event_loop()
+loop.create_task(server_loop(loop, server_sock))
+loop.run_forever()
